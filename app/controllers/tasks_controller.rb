8301eaf3a05f210ebb0a96f901b015d60f022bef
yuya.sugiuchi <yuya.zerostart@gmail.com>
2020-03-17 11:20:14 +0000

    kadai-syusei4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
     
     before_action :require_user_logged_in 
-    before_action :correct_user, only:[:edit, :update, :destroy]
+    before_action :correct_user, only:[:show, :edit, :update, :destroy]
     
     def index
         if logged_in?
